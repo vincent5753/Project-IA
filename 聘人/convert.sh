30192173a79d983e76df000756db1a72e6e0d1f7
VP <51seer.vincent@gmail.com>
2023-09-22 03:03:22 +0800

    新增 convert.sh 方便掃描相關轉檔案
    Signed-off-by: VP <51seer.vincent@gmail.com>

---
+++ 聘人/convert.sh
@@ -0,0 +1,14 @@
+function 健康(){
+  echo 健康
+  convert "*.jpg" -resize 3612x5125 -quality 1 x-健康聲明書.pdf
+}
+
+function 型態(){
+  echo 型態
+  convert 1.jpg -quality 1 x-型態.pdf
+}
+
+function 學習(){
+  echo 學習
+  convert "*.jpg" -quality 5 x-學習指導實施計劃.pdf
+}
